11d06062303540ab212c5c35411a2b55c4fc6bb4
deekshasharma <deeksha.sharma25@gmail.com>
2021-02-23 16:07:01 -0800

    updated asset location

--- src/db/store.js
+++ src/db/store.js
@@ -5,79 +5,78 @@ export const skuToUrl = [
   },
   {
     sku: 'set-of-4-hand-drawn-vehicles-poster-1614012524115',
-    path: '/static/posters/vehicles.zip'
+    path: '/static/vehicles.zip'
   },
   {
     sku: 'venice-1614114095382',
-    path: '/static/posters/venice.jpg'
+    path: '/static/venice.jpg'
   },
   {
     sku: 'birds-on-stem-1614113869721',
-    path: '/static/posters/birds_on_stem.jpg'
+    path: '/static/birds_on_stem.jpg'
   },
   {
     sku: 'journey-that-matters-1614088347120',
-    path: '/static/posters/journey.jpg'
+    path: '/static/journey.jpg'
   },
   {
     sku: 'things-that-matter-1614014223209',
-    path: '/static/posters/things_that_matter.jpg'
+    path: '/static/things_that_matter.jpg'
   },
   {
     sku: 'believe-1614014165545',
-    path: '/static/posters/believe.jpg'
+    path: '/static/believe.jpg'
   },
   {
     sku: 'persistence-1614014091688',
-    path: '/static/posters/persistence.jpg'
+    path: '/static/persistence.jpg'
   },
   {
     sku: 'unstoppable-1614013944078',
-    path: '/static/posters/unstoppable.jpg'
+    path: '/static/unstoppable.jpg'
   },
   {
     sku: 'dancing-lady-1614108790820',
-    path: '/static/posters/dance.png'
+    path: '/static/dance.png'
   },
   {
     sku: 'dog-love-1614108572586',
-    path: '/static/posters/dog_love.png'
+    path: '/static/dog_love.png'
   },
   {
     sku: 'deer-1614116403166',
-    path: '/static/posters/deer.JPG'
+    path: '/static/deer.JPG'
   },
   {
     sku: 'rainforest-1614116290805',
-    path: '/static/posters/rainforest.JPG'
+    path: '/static/rainforest.JPG'
   },
   {
     sku: 'krishna-1614116161704',
-    path: '/static/posters/krishna.JPG'
+    path: '/static/krishna.JPG'
   },
   {
     sku: 'single-bird-1614116028228',
-    path: '/static/posters/single_bird.JPG'
+    path: '/static/single_bird.JPG'
   },
   {
     sku: 'dream-big-1614108222985',
-    path: '/static/posters/dream_big.jpg'
+    path: '/static/dream_big.jpg'
   },
   {
     sku: 'vintage-telephone-poster-1614011531296',
-    path: '/static/posters/telephone.jpg'
+    path: '/static/telephone.jpg'
   },
   {
     sku: 'abstract-geometry-1614088264316',
-    path: '/static/posters/abstract.jpg'
+    path: '/static/abstract.jpg'
   },
   {
     sku: 'vintage-cars-1614087778396',
-    path: '/static/posters/vintage_cars.jpg'
+    path: '/static/vintage_cars.jpg'
   },
   {
     sku: 'journey-that-matters-1614100020615',
-    path: '/static/posters/journey.jpg'
-  },
-
+    path: '/static/journey.jpg'
+  }
 ]
