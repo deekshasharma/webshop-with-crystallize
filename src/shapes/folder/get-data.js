08d5102f5a252f9574c8f49e38aec03d5a2eb209
deeksha-crys <deeksha@crystallize.com>
2021-09-29 13:58:09 -0700

    calls service-api to get the folder data

--- src/shapes/folder/get-data.js
+++ src/shapes/folder/get-data.js
@@ -1,8 +1,9 @@
-import { simplyFetchFromGraph } from 'lib/graph';
+import { simplyFetchFromGraph2 } from 'lib/graph';
 import { QUERY_FOLDER } from './query';
 
 export async function getData({ asPath, language, preview = null }) {
-  const { data } = await simplyFetchFromGraph({
+  const { data } = await simplyFetchFromGraph2({
+    uri:`${process.env.NEXT_PUBLIC_SERVICE_NON_GRAPHQL_API_URL}/api/products/get-folder`,
     query: QUERY_FOLDER,
     variables: {
       path: asPath,
